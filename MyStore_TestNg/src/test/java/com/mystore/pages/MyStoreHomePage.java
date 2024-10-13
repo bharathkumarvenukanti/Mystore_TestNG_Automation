74a0798ef43b1288415fd92d3564696040eaef7c
bhara <bhara@Bharath>
2024-10-13 15:17:13 +0300

    Code Added

---
+++ MyStore_TestNg/src/test/java/com/mystore/pages/MyStoreHomePage.java
@@ -0,0 +1,5 @@
+package com.mystore.pages;
+
+public class MyStoreHomePage {
+
+}
---
+++ MyStore_TestNg/src/test/java/com/mystore/testcases/MystoreHomepageTest.java
@@ -0,0 +1,9 @@
+package com.mystore.testcases;
+
+import org.testng.annotations.Test;
+
+public class MystoreHomepageTest {
+  @Test
+  public void f() {
+  }
+}
