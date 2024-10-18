0f191f8c6b5dba176c94e85af87f4bd0580bbe9f
bhara <bhara@Bharath>
2024-10-18 14:34:14 +0300

    Code Added

--- MyStore_TestNg/src/test/java/com/mystore/testcases/HomepageNavigation.java
+++ MyStore_TestNg/src/test/java/com/mystore/testcases/HomepageNavigation.java
@@ -3,8 +3,18 @@ package com.mystore.testcases;
 import org.testng.annotations.Test;
 
 import com.mystore.driverscript.TestBase;
-
+/**
+ * This class represents the HomepageNavigation test case for the "mystore" module.
+ * It extends the TestBase class, which provides common test setup and teardown functionality.
+ */
 public class HomepageNavigation extends TestBase  {
+	/**
+     * This test method launches the homepage of the "mystore" application.
+     *
+     * It checks the "module" property in the configuration file. If it's equal to "mystore",
+     * it navigates to the URL specified in the "mystoreurl" property. Otherwise,
+     * it prints an error message indicating an issue with the URL.
+     */
   @Test
   public void LaunchHomepage(){
 		if(config.getProperty("module").equalsIgnoreCase("mystore")) {
--- MyStore_TestNg/src/test/java/com/mystore/testcases/MystoreHomepageTest.java
+++ MyStore_TestNg/src/test/java/com/mystore/testcases/MystoreHomepageTest.java
@@ -11,41 +11,52 @@ import com.mystore.pages.MyStoreHomePageLogo;
 import com.mystore.utilities.TestUtil;
 
 public class MystoreHomepageTest extends TestBase {
-  public HomepageNavigation homePage;
-  public MyStoreTitlesRepository Titlesrepo;
-  public TestUtil utils;
-  public MyStoreHomePageLogo Logo;
-  
-  //Declaring titles
-  public String MystoreExpectedTitle=Titlesrepo.MystoreHomePageTitle;
+	// Declare page and utility instances
+	public HomepageNavigation homePage;
+	public MyStoreTitlesRepository Titlesrepo;
+	public TestUtil utils;
+	public MyStoreHomePageLogo Logo;
+
+	//Declaring titles
+	public String MystoreExpectedTitle=Titlesrepo.MystoreHomePageTitle;
 
 	@BeforeMethod
-  public void VerifyMystoreHomepage() {
+	public void VerifyMystoreHomepage() {
+		// Initialize utility and page objects
 		utils=new TestUtil();
 		homePage=new HomepageNavigation();
 		Logo=PageFactory.initElements(driver, MyStoreHomePageLogo.class);
+		// Launch homepage
 		homePage.LaunchHomepage();
+		// Get actual homepage title
 		String ActualHomepageTitle=driver.getTitle();
+		// Verify homepage title using TestUtil
 		utils.AssrtEquals(ActualHomepageTitle, MystoreExpectedTitle, "Title are not matched");
-  }
+	}
 	@Test
 	public void verifyMystoreHomePageLogo() {
+		// Check if logo is displayed
 		//Logo.isLogoDisplayed();
 		boolean islogo=Logo.isLogoDisplayed();
 		try {
+			// Verify logo is displayed using TestUtil
 			utils.AssrtTrue(islogo, "Title is not displayed");
+			// Click on logo
 			Logo.clickLogo();
+			// Verify title after clicking logo
 			utils.AssrtEquals(driver.getTitle(), MystoreExpectedTitle, "Title are not matched");
+			// Verify logo redirects to homepage with expected title
 			Logo.LogoRedirectsToHomepage(MystoreExpectedTitle);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			// Handle potential interruption exception
 			e.printStackTrace();
 		}
 	}
 	@AfterMethod
 	public void Assertall() {
-		
+		// Assert all soft assertions from TestUtil
+
 		utils.assertAllSoftAssertions();
 	}
-	
+
 }
